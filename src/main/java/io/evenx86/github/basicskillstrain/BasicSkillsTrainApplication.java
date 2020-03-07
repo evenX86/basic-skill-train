9849c593365e1eaef85cd82a20c2009c47489f36
mtdp <mtdp@mtdpdeMacBook-Pro.local>
2020-03-07 22:47:32 +0800

    initial commit

---
+++ src/main/java/io/evenx86/github/basicskillstrain/BasicSkillsTrainApplication.java
@@ -0,0 +1,13 @@
+package io.evenx86.github.basicskillstrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSkillsTrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSkillsTrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/evenx86/github/basicskillstrain/BasicSkillsTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package io.evenx86.github.basicskillstrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSkillsTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
